a47e18e9922595af94b498380a5cd9a222fdc555
RevazRevazashvili <revazi.revazashvili380@ens.tsu.edu.ge>
2024-02-12 21:45:13 +0400

    modified main.py

--- main.py
+++ main.py
@@ -1,34 +1,49 @@
 import requests
 from googletrans import Translator
-from openai import OpenAI
+import openai
 
-client = OpenAI(
-    api_key= 
-)
+api_key = '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'
+
+openai.api_key = api_key
 
 API_URL = "https://api-inference.huggingface.co/models/m3hrdadfi/wav2vec2-large-xlsr-georgian"
 headers = {"Authorization": "Bearer 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"}
 
+
 def query(filename):
     with open(filename, "rb") as f:
         data = f.read()
-    response = requests.post(API_URL, headers=headers, data=data)
-    return response.json()
+    resp = requests.post(API_URL, headers=headers, data=data)
+    return resp.json()
 
-output = query("sounds/Recording.mp3")
 
-translator = Translator()
-translation = translator.translate(output, dest="en")
+def voice_to_text(file):
+    output = query(file)
+    output = next(iter(output.values()))
+    return output
 
-print(translation.text)
 
-chat_completion = client.chat.completions.create(
+def translator_lan(text, lang_from, lang_to):
+    translator = Translator()
+    translation = translator.translate(text, src=lang_from, dest=lang_to)
+    return translation.text
+
+text = voice_to_text("sounds/Recording.mp3")
+
+translated = translator_lan(text, "ka", "en")
+
+print(translated)
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
     messages=[
         {
             "role": "user",
-            "content": "write me notes from this text",
+            "content": translated+"can u write note from this text?"
         }
-    ],
-    model="gpt-3.5-turbo",
+    ]
 )
-print(chat_completion)
+
+note = response['choices'][0]['message']['content']
+
+print(note)
